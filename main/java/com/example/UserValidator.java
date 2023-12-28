a0115a5a3d51093bbfa0016c45f5edd061a67508
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-28 18:13:45 +0530

    Implemented UC2

--- main/java/com/example/UserValidator.java
+++ main/java/com/example/UserValidator.java
@@ -7,6 +7,8 @@ public class UserValidator {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
+
+        //First name
         System.out.println("Enter the first name:");
 
         String first_name = sc.nextLine();
@@ -14,10 +16,23 @@ public class UserValidator {
 
         // Pattern pattern = Pattern.compile(valid_first_name);
         // Matcher matcher = pattern.matcher(first_name);
-        
-        System.out.println("Valid First Name: " + patternValidator(first_name, valid_first_name));
-        
+        boolean valid = patternValidator(first_name, valid_first_name);
+        System.out.println("Valid First Name: " + valid);
+        System.out.println("");
+        if (!valid) 
+            return;
 
+        //Last name
+        System.out.println("Enter the last name:");
+
+        String last_name = sc.nextLine();
+        String valid_last_name = "^[A-Z][a-zA-Z]{2,}$";
+        valid = patternValidator(last_name, valid_last_name);
+        System.out.println("Valid Last Name: " + patternValidator(last_name, valid_last_name));
+        if(!valid)
+            return;
+
+        
     }
 
     public static boolean patternValidator(String text, String valid_text)

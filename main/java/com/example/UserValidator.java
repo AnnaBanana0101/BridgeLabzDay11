12e10d03bb0a13ea06e5ee7f4971adcdc4182603
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-28 18:07:50 +0530

    Implemented UC1

--- main/java/com/example/UserValidator.java
+++ main/java/com/example/UserValidator.java
@@ -12,11 +12,19 @@ public class UserValidator {
         String first_name = sc.nextLine();
         String valid_first_name = "^[A-Z][a-zA-Z]{2,}$";
 
-        Pattern pattern = Pattern.compile(valid_first_name);
-        Matcher matcher = pattern.matcher(first_name);
-
-        System.out.println("Valid First Name: " + matcher.matches());
+        // Pattern pattern = Pattern.compile(valid_first_name);
+        // Matcher matcher = pattern.matcher(first_name);
+        
+        System.out.println("Valid First Name: " + patternValidator(first_name, valid_first_name));
         
 
+    }
+
+    public static boolean patternValidator(String text, String valid_text)
+    {
+        Pattern pattern = Pattern.compile(valid_text);
+        Matcher matcher = pattern.matcher(text);
+
+        return matcher.matches();
     }
 }
